ea61ee755b9d873fd78bdee7efee4c09130324a4
Manassés Ribeiro <manasses1979@gmail.com>
2020-06-23 19:56:52 -0300

    Add files via upload

---
+++ mapa_calor.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from numpy import genfromtxt
+
+for i in xrange(91):
+	if (i % 9 == 0):	
+		m = genfromtxt(('heat_%s.csv' % i), delimiter=',')
+	
+		fig = plt.figure(figsize=(6, 3.2))
+		ax = fig.add_subplot(111)
+		ax.set_title(('heat_%s' % i))
+		ax.set_aspect('equal')
+
+		plt.imshow(m)
+		plt.clim(0,1)
+		plt.colorbar(orientation='vertical')
+		plt.savefig(('heat_%s.jpg' % i))
+#		plt.show()
